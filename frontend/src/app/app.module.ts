6dfff912cf6b201789b23675823bbee43c9f5d63
oPuDDingo <valerie.morviller@student.fhws.de>
2022-01-14 19:40:08 +0100

    implement toolbar with logo

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
 import { ListComponent } from './list/list.component';
 import { ReminderComponent } from './reminder/reminder.component';
+import { ToolbarComponent } from "../lib/ui/toolbar/toolbar.component";
 
 @NgModule({
-  declarations: [AppComponent, BoardComponent, ListComponent, ReminderComponent],
+  declarations: [AppComponent, BoardComponent, ListComponent, ReminderComponent, ToolbarComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
