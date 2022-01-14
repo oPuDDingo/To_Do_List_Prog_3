6e8b28e5769bdc1154ca4bb32d405a6e51b3696d
oPuDDingo <valerie.morviller@student.fhws.de>
2022-01-14 18:34:07 +0100

    implement models and create board, list and reminder component, change icon

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { ListComponent } from './list/list.component';
+import { ReminderComponent } from './reminder/reminder.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BoardComponent, ListComponent, ReminderComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
